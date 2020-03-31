3410c832d0cab1afb425b3df00d9442f21b35914
SarahF <sarahfrerichs@gmail.com>
2020-03-31 00:50:16 -0500

    added blue "more info" box

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -13,15 +13,30 @@ export type IHomeProp = StateProps;
 export const Home = (props: IHomeProp) => {
   const { account } = props;
 
+function moreInfo() {
+  document.getElementById("infoBox").removeAttribute("hidden");
+}
+
   return (
     <Row>
       <Col md="6">
         <div className="container p-3 my-3 bg-dark">
           <h1 className="display-3">One Thing at a Time App</h1>
           <p className="lead">Helps you get things done by focusing on one thing at a time!</p>
-          <p className="lead">
-              <a className="btn btn-info btn-lg" href="#" role="button">Learn more</a>
+          <hr className="my-4" />
+
+          <span hidden id="infoBox">
+            <div className="alert alert-dismissible alert-info">
+              <strong>Heads up!</strong> This alert needs your attention, but its not super important.
+            </div>
+          </span>
+
+            <p className="lead">
+                <a className="btn btn-info btn-lg"
+                onClick={()=>moreInfo()}
+                role="button">Learn more</a>
             </p>
+
         </div>
       </Col>
 
@@ -37,15 +52,15 @@ export const Home = (props: IHomeProp) => {
                 {' '}
                 Sign in here
               </Link>
-              <p></p>
-              <p>or</p>
+              <p />
               <Link to="/account/register" className="alert-link">
                 Register a new account
               </Link>
-
-              <p>For development try these: </p>
+              <p />
+              <div>For development try these:
               <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
               <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
+              </div>
             </Alert>
           </div>
         )}
